8e50cc560a69af78ed187046ef824f13ab1cace1
bhaskar-tripathi-coder <bt2401@srmist.edu.in>
2026-03-10 08:15:03 +0530

    [bhaskar-tripatho-coder]

---
+++ UC5_1.java
@@ -0,0 +1,63 @@
+public class UC5_1 {
+
+    public static void main(String[] args) {
+
+        // Declare and initialize array in one statement
+        String[] banner = {
+
+                String.join("",
+                        "  *****  ",
+                        "  *****  ",
+                        "  ****** ",
+                        "  ****** "
+                ),
+
+                String.join("",
+                        " *     * ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * "
+                ),
+
+                String.join("",
+                        " *     * ",
+                        " *     * ",
+                        " *     * ",
+                        " *       "
+                ),
+
+                String.join("",
+                        " *     * ",
+                        " *     * ",
+                        " ******  ",
+                        "  *****  "
+                ),
+
+                String.join("",
+                        " *     * ",
+                        " *     * ",
+                        " *       ",
+                        "       * "
+                ),
+
+                String.join("",
+                        " *     * ",
+                        " *     * ",
+                        " *       ",
+                        " *     * "
+                ),
+
+                String.join("",
+                        "  *****  ",
+                        "  *****  ",
+                        " *       ",
+                        "  *****  "
+                )
+        };
+
+        // Print using enhanced for-loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
